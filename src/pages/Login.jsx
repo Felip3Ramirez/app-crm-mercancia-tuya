ac626f9286160e201f53b79b52492efd29ac5e8c
Sahara Ṕalomino <saharapcq@hotmail.com>
2025-04-23 14:46:57 -0500

    mi primera peticion get

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,48 +1,77 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
-import { alertaGenerica, alertaRedireccion, generarToken } from "../helpers/funcione";
+import {
+  alertaGenerica,
+  alertaRedireccion,
+  generarToken,
+} from "../helpers/funcione";
+
+let apiUsuarios = "https://back-json-server-tuya.onrender.com/usuarios";
+
 function Login() {
-   const [name, setName] = useState("")
-    const [password, setPassword] = useState("")
-    const [getHoraLogin, setHoraLogin] = useState("")
-    // const [usuarios, setUsuarios] = useState([])
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [getHoraLogin, setHoraLogin] = useState("");
+  const [usuarios, setUsuarios] = useState([]);
 
-    let redireccion = useNavigate()
-    
-    
+  let redireccion = useNavigate();
 
-  // function getUsuarios(){
-  //   fetch("http://localhost:3001/usuarios",{})
-  //   .then(Response => Response.json())
-  //   .then(data =>  setUsuarios(data)
-  // )
-  // }
-  // getUsuarios()
-  // console.log(usuarios);
+  function getUsuarios() {
+    fetch(apiUsuarios)
+      .then((Response) => Response.json())
+      .then((data) => setUsuarios(data))
+      .catch((error) => console.log(error));
+      
+  }
+  
+  useEffect(() => {
+    getUsuarios();
+  },[]);
 
-    function iniciarSesion (user,password){
-        if(user === "admin" && password==="123456"){
-          setHoraLogin(new Date().toLocaleDateString())
-          let horaInicio = new Date();
-          console.log(horaInicio);
-          let tokenAcceso = generarToken();
-          localStorage.setItem("token",tokenAcceso);
-          alertaRedireccion(redireccion,"Bienvenido","Sera redieccionado al Home","success","/home");
-        }else{
-          alertaGenerica("Error","Usuario o contraseña incorrectos","error");
-        }
-        
+  function buscarUsuario() {
+    let user = usuarios.find((item) => name == item.user && password == item.password);
+    return user;
+  }
+  
+
+  function iniciarSesion() {
+    if (buscarUsuario()) {
+      setHoraLogin(new Date().toLocaleDateString());
+      let horaInicio = new Date();
+      console.log(horaInicio);
+      let tokenAcceso = generarToken();
+      localStorage.setItem("token", tokenAcceso);
+      alertaRedireccion(
+        redireccion,
+        "Bienvenido",
+        "Sera redieccionado al Home",
+        "success",
+        "/home"
+      );
+    } else {
+      alertaGenerica("Error", "Usuario o contraseña incorrectos", "error");
     }
+  }
 
-    
-    
   return (
     <form className="form">
-        Sing Up
-      <input onChange={(e) => setName(e.target.value)} type="text" className="input" placeholder="Name" />
-      <input onChange={(e) => setPassword(e.target.value)} type="text" className="input" placeholder="Password" />
-      <button type="button" onClick={()=> iniciarSesion(name,password)}>Submit</button>
+      Sing Up
+      <input
+        onChange={(e) => setName(e.target.value)}
+        type="text"
+        className="input"
+        placeholder="Name"
+      />
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        type="text"
+        className="input"
+        placeholder="Password"
+      />
+      <button type="button" onClick={iniciarSesion}>
+        Submit
+      </button>
     </form>
   );
 }
